cb00e463786f1e0b4cffcb9167db9a1c76810398
Rahmat Hidayat <kickyou44@gmail.com>
2019-03-14 05:29:02 +0700

    useCountdown function

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+export interface CountdownOptions {
+  readonly targetTime: number
+  readonly interval?: number
+}
+
+export const useCountdown: (options: CountdownOptions) => number = ({
+  targetTime,
+  interval = 1000,
+}) => {
+  const [ms, setMs] = useState<number>(targetTime - Date.now())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const delta = targetTime - Date.now()
+      if (delta > 0) {
+        setMs(delta)
+      } else {
+        setMs(0)
+        clearInterval(intervalId)
+      }
+    }, interval)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [targetTime, interval])
+
+  return ms
+}
